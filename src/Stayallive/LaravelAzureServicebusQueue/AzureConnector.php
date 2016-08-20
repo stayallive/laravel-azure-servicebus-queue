41ae4079f4ddc7fbcded2e26cd62f74629a9d610
Alex Bouma <me@alexbouma.me>
2016-08-20 19:17:15 +0000

    Applied fixes from StyleCI

--- src/Stayallive/LaravelAzureServicebusQueue/AzureConnector.php
+++ src/Stayallive/LaravelAzureServicebusQueue/AzureConnector.php
@@ -6,20 +6,21 @@ use Illuminate\Http\Request;
 use WindowsAzure\Common\ServicesBuilder;
 use Illuminate\Queue\Connectors\ConnectorInterface;
 
-class AzureConnector implements ConnectorInterface {
+class AzureConnector implements ConnectorInterface
+{
 
-	/**
-	 * Establish a queue connection.
-	 *
-	 * @param  array $config
-	 *
-	 * @return \Illuminate\Queue\QueueInterface
-	 */
-	public function connect(array $config) {
+    /**
+     * Establish a queue connection.
+     *
+     * @param  array $config
+     *
+     * @return \Illuminate\Queue\QueueInterface
+     */
+    public function connect(array $config)
+    {
         $connectionString = 'Endpoint=' . $config['endpoint'] . ';SharedSecretIssuer=' . $config['secretissuer'] . ';SharedSecretValue=' . $config['secret'];
-		$serviceBusRestProxy = ServicesBuilder::getInstance()->createServiceBusService($connectionString);
+        $serviceBusRestProxy = ServicesBuilder::getInstance()->createServiceBusService($connectionString);
 
-		return new AzureQueue($serviceBusRestProxy, $config['queue']);
-	}
-
+        return new AzureQueue($serviceBusRestProxy, $config['queue']);
+    }
 }
